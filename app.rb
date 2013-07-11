be503abfdbd7bf979a8f02b8066707244953692f
Paulo Schneider <pschneid@thoughtworks.com>
2013-07-11 14:55:48 +0100

    Making the webapp routes a bit smarter

--- app.rb
+++ app.rb
@@ -1,10 +1,79 @@
 require 'sinatra'
+require 'csv'
+require 'json'
 
+# Webapp
 
-get '/map' do
+get '/map/:phase' do
+  @phase = params[:phase]
+
   erb :map
 end
 
-get '/release_d3.json' do
-  File.read('release_d3.json') 
+get '/:phase.json' do
+  File.read("#{params[:phase]}.json") 
+end
+
+# "Model"
+
+class Story
+  attr_reader :category, :status, :estimate
+
+  def initialize(category, name, estimate, status)
+    @category, @name, @estimate, @status = category, name, estimate, status 
+  end
+
+  def description
+    "[#@status] #@category - #@name (size #@estimate)"
+  end
+  
+  def size_for_map
+    estimate + 1
+  end
+
+end
+
+
+# Utils
+
+def json_for(name)
+  {
+    "name" => name,
+    "children" => []
+  }
+end
+
+all_stories = CSV.readlines('release.csv').drop(1).map do |row|
+  status = row[3]
+  status = "Not Started" if status.nil?
+
+  estimate = row[2]
+  if row[2].nil?
+    estimate = 0
+  else
+    estimate = row[2].to_i
+  end
+
+  Story.new(row[0], row[1], estimate, status)
+end
+
+d3_json = json_for("release")
+
+all_stories.group_by(&:status).each do |status, status_stories|
+  status_json = json_for(status)
+
+  status_stories.group_by(&:category).each do |category, stories|
+
+    stories.each do |story|
+      story_json = { "name" => story.description, "size" => story.size_for_map }
+
+      status_json["children"] << story_json
+    end
+  end
+
+  d3_json["children"] << status_json
+end
+
+File.open("release_d3.json","w") do |f|
+  f.write(d3_json.to_json)
 end
