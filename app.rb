26a1156d8f2e34cd715998adc7d5502268966b25
Paulo Schneider <pschneid@thoughtworks.com>
2013-07-11 16:04:01 +0100

    A bit of cleaning

--- app.rb
+++ app.rb
@@ -2,23 +2,32 @@ require 'sinatra'
 require 'csv'
 require 'json'
 
-# Webapp
 
+# From lighter (Done) to darker (Not Estimated)
 COLORS = {
   "green" => "'#CAF6D8', '#96EEB0', '#57E482', '#2CDD61', '#1DAF49'",
   "blue" => "'#BDEFEF', '#8CE3E3', '#52D5D5', '#30C5C5', '#249494'",
   "orange" => "'#FFE9D6', '#FFC799', '#FFA962', '#FF841F', '#E06500'"
 }
 
+# Webapp
+
+get '/' do
+  erb :instructions
+end
+
 get '/map/:phase' do
   @phase = params[:phase]
-  color = params.key?("color") ? params["color"] : "green"
-  @color_scheme = COLORS[color]
+  @color_scheme = COLORS[params.fetch("color", "green")]
+  @map_mode = params.fetch("map_mode", "squarify")
 
   erb :map
 end
 
 get '/:phase.json' do
+  # Hackily parse the stories from a csv file and transfom the rows
+  # into a D3 treemap compatible json object.
+
   @phase = params[:phase]
   csv_file = "#@phase.csv"
 
@@ -67,7 +76,7 @@ class Story
   end
 
   def description
-    "[#@status] #@category - #@name (size #@estimate)"
+    "[#@status] #@category - #@name (estimate #@estimate)"
   end
   
   def size_for_map
