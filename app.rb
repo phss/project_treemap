95914c789f14d8b276a571622f881d46b69b62cc
Paulo Schneider <pschneid@thoughtworks.com>
2013-07-11 15:02:18 +0100

    Generating json on the fly

--- app.rb
+++ app.rb
@@ -11,7 +11,41 @@ get '/map/:phase' do
 end
 
 get '/:phase.json' do
-  File.read("#{params[:phase]}.json") 
+  @phase = params[:phase]
+
+  all_stories = CSV.readlines("#@phase.csv").drop(1).map do |row|
+    status = row[3]
+    status = "Not Started" if status.nil?
+  
+    estimate = row[2]
+    if row[2].nil?
+      estimate = 0
+    else
+      estimate = row[2].to_i
+    end
+  
+    Story.new(row[0], row[1], estimate, status)
+  end
+
+  d3_json = json_for("release")
+  
+  all_stories.group_by(&:status).each do |status, status_stories|
+    status_json = json_for(status)
+  
+    status_stories.group_by(&:category).each do |category, stories|
+  
+      stories.each do |story|
+        story_json = { "name" => story.description, "size" => story.size_for_map }
+  
+        status_json["children"] << story_json
+      end
+    end
+  
+    d3_json["children"] << status_json
+  end
+
+  content_type :json
+  d3_json.to_json
 end
 
 # "Model"
@@ -41,39 +75,4 @@ def json_for(name)
     "name" => name,
     "children" => []
   }
-end
-
-all_stories = CSV.readlines('release.csv').drop(1).map do |row|
-  status = row[3]
-  status = "Not Started" if status.nil?
-
-  estimate = row[2]
-  if row[2].nil?
-    estimate = 0
-  else
-    estimate = row[2].to_i
-  end
-
-  Story.new(row[0], row[1], estimate, status)
-end
-
-d3_json = json_for("release")
-
-all_stories.group_by(&:status).each do |status, status_stories|
-  status_json = json_for(status)
-
-  status_stories.group_by(&:category).each do |category, stories|
-
-    stories.each do |story|
-      story_json = { "name" => story.description, "size" => story.size_for_map }
-
-      status_json["children"] << story_json
-    end
-  end
-
-  d3_json["children"] << status_json
-end
-
-File.open("release_d3.json","w") do |f|
-  f.write(d3_json.to_json)
 end
